0ebe1f8a9ef5640cbac3e553a7bba825a3a7feda
ORLANDO RUBIO RIVAS <orlando.rubio@HELISA.LOCAL>
2018-09-27 11:49:35 -0500

    se inhabilita que el jpa escuche

--- usuario/src/main/java/com/example/usuario/UsuarioApplication.java
+++ usuario/src/main/java/com/example/usuario/UsuarioApplication.java
@@ -2,8 +2,11 @@ package com.example.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UsuarioApplication {
 
 	public static void main(String[] args) {
